998131d9db88ac3a55c91103745be3cbec38e9e6
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-21 14:41:47 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/homepage', function () {
+    return view('homepage');
+})->name('homepage');
+
+Route::get('/campus-directory', function () {
+    return view('campus-directory');
+})->name('campus.directory');
+
+Route::get('/campus-policies', function () {
+    return view('campus-policies');
+})->name('campus.policies');
+
+Route::get('/submit-ticket', function () {
+    return view('submit-ticket');
+})->name('submit.ticket');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
