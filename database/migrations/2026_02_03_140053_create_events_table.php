38477d96b3962a441a8048c81c3112273cc727cd
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-03 22:01:18 +0800

    Update 2026_02_03_140053_create_events_table.php

--- database/migrations/2026_02_03_140053_create_events_table.php
+++ database/migrations/2026_02_03_140053_create_events_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
