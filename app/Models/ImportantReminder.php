1ea807068e82c3e37714849e6857600cbe4bd83f
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-09 21:51:57 +0800

    Update ImportantReminder.php

--- app/Models/ImportantReminder.php
+++ app/Models/ImportantReminder.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ImportantReminder extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
 }
