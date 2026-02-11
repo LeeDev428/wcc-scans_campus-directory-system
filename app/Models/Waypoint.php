17136d92814276215ceb6e2f8713c176beef3891
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-12 01:21:52 +0800

    Update Waypoint.php

--- app/Models/Waypoint.php
+++ app/Models/Waypoint.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Waypoint extends Model
 {
-    //
+    protected $fillable = ['floor', 'x', 'y', 'type'];
+    
+    protected $casts = [
+        'floor' => 'integer',
+        'x' => 'float',
+        'y' => 'float'
+    ];
+    
+    // Relationship to connected waypoints
+    public function connections()
+    {
+        return $this->belongsToMany(Waypoint::class, 'waypoint_connections', 'from_waypoint_id', 'to_waypoint_id')
+                    ->withPivot('distance')
+                    ->withTimestamps();
+    }
 }
