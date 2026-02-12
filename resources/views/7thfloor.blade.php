40891b3be960955cf486c09d2b27c83d258dcf8c
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-12 10:11:39 +0800

    Update 7thfloor.blade.php

--- resources/views/7thfloor.blade.php
+++ resources/views/7thfloor.blade.php
@@ -37,6 +37,9 @@
 <body style="background: linear-gradient(90deg, #164D30 0%, #185336 60%, #369976 100%);">
     <!-- Floor Navigator Component -->
     <x-floor-navigator :currentFloor="7" />
+    
+    <!-- Room Search Component -->
+    <x-room-search />
 
     <!-- Main Content -->
     <div class="floor-container">
